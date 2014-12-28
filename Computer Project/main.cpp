8b9dea1f71c840c77a577b678d103f5669b74600
Samar Sunkaria <Samar@Samars-MacBook-Air.local>
2014-12-28 12:54:17 +0530

    Initial Commit

---
+++ Computer Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Computer Project
+//
+//  Created by Samar Sunkaria on 12/28/14.
+//  Copyright (c) 2014 Samar Sunkaria. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
